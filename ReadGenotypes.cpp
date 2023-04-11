39991870599309fc23c7f10e478efafaa92757d7
nvluke <130038150+nvluke@users.noreply.github.com>
2023-04-11 10:08:52 +0300

    Update ReadGenotypes.cpp

--- ReadGenotypes.cpp
+++ ReadGenotypes.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <chrono>
+#include <algorithm>
+#include <cctype>
 
 // [[Rcpp::plugins(cpp17)]] 
 
@@ -20,7 +22,7 @@ List ReadGeno(const string & filename){
   string   mij; 
   int      pos1;
   int      pos2;
-  int      posn;
+  //int      posn;
   int      i = 0;
   int      j = 0;
   int      k = 0;
@@ -41,14 +43,10 @@ List ReadGeno(const string & filename){
     if(n == 0){
       start = line.find(delim);
       end   = line.size();
-      nump  = line.find_first_of("0123456789", start);
-      pos1  = start + delim.size();
-      pos2  = end;
-      posn  = nump;
-      if(pos1 < posn){
-        pos1 = posn;
-      }
-      m     = pos2 - pos1;
+      nump  = line.find_first_of("0123456789", 0);
+      pos1  = line.find(delim, nump) + delim.size();
+      pos2  = line.find_first_of("0123456789", pos1);
+      m     = end - pos2;
     }
     n++;
   }
@@ -67,11 +65,14 @@ List ReadGeno(const string & filename){
   while(getline(infile, line)){
     start  = line.find(delim);
     end    = line.size();
-    nump   = line.find_first_of("0123456789", start);
-    nameid = line.substr(0, start);
+    nump   = line.find_first_of("0123456789", 0);
+    pos1   = line.find(delim, nump) + delim.size();
+    pos2   = line.find_first_of("0123456789", pos1);
+    nameid = line.substr(0, pos1);
+    nameid.erase(std::remove_if(nameid.begin(), nameid.end(), ::isspace), nameid.end());
     ID[i]  = nameid; // Save animal ID in vector
     k      = 0;
-    for(j = nump; j < end; j++){
+    for(j = pos2; j < end; j++){
       // Saving genotypes into matrix
       mij     = line[j];
       M(i, k) = stoi(mij);
